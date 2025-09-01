c538446371d0734ecf0662580b5f70b8ca9c1376
PedroPsousilva <pedro.soupsilva@gmail.com>
2025-09-01 12:53:23 -0300

    01-09

---
+++ src/homeScreen.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { View, Text, Button } from "react-native"; // sem espaço + adiciona Button
+import { useNavigation } from "@react-navigation/native";
+
+import {useState} from "react";
+import api from "./axios/axios";
+
+const HomeScreen = () => {
+  const [stateLED, setStateLED] = useState({value: false});
+  const navigation = useNavigation();
+
+  const handleCam = () => {
+    navigation.navigate("Cam");
+    
+  };
+  async function ledToggle(){
+    try{
+      // Faz a requisição da API do Adafruit
+      const response = await api.toggleLED({value: `${!stateLED.value}`});
+      setStateLED({value: !stateLED.value})
+      console.log("Resposta: ",response.data);
+    }
+    catch(error){
+      console.log("Erro", error.response.data);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={{marginBottom: 20}}>
+      <Button title="Abrir Camera" onPress={handleCam} color="blue" />
+      </View>
+      <View>
+        <Button title={stateLED.value ? "Desligar LED":"Ligar LED"}
+        onPress={ledToggle}
+        color={stateLED.value ? "red": "green"}/>
+      </View>
+    </View>
+    
+  );
+};
+
+export default HomeScreen;
